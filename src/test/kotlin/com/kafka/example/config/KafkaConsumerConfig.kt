a190be7538e814e289b0cda86d876e42cb520d9c
Author Name <jonathan.lindquist@protonmail.com>
2023-01-12 18:06:09 -0600

    removing overridden configs

--- src/test/kotlin/com/kafka/example/config/KafkaConsumerConfig.kt
+++ src/test/kotlin/com/kafka/example/config/KafkaConsumerConfig.kt
@@ -1,12 +1,9 @@
 package com.kafka.example.config
 
-import com.kafka.example.KafkaServerTestProvider
 import com.kafka.example.consumer.AbstractReactiveKafkaConsumer
 import com.kafka.example.dto.BaseDTO
-import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.util.UUID
 import java.util.logging.Logger
 
 @Configuration
@@ -16,14 +13,8 @@ class KafkaConsumerConfig {
 
         override val topics = listOf("example-reactive-topic")
 
-        override fun kafkaConsumerProperties(): MutableMap<String, Any> =
-            mutableMapOf(
-                Pair(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaServerTestProvider.KAFKA_CONTAINER.bootstrapServers),
-                Pair(ConsumerConfig.GROUP_ID_CONFIG, "group-id-${UUID.randomUUID()}"),
-                Pair(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"),
-            )
-
-        override suspend fun <T : Any> accept(dto: T) = logger.info("overridden accept method: ${this::class.simpleName}")
+        override suspend fun <T : Any> accept(dto: T) =
+            logger.info("overridden accept method: ${this::class.simpleName}")
     }
 
     @Bean
--- src/test/kotlin/com/kafka/example/config/KafkaProducerConfig.kt
+++ src/test/kotlin/com/kafka/example/config/KafkaProducerConfig.kt
@@ -1,13 +1,10 @@
 package com.kafka.example.config
 
-import com.kafka.example.KafkaServerTestProvider
 import com.kafka.example.dto.BaseDTO
 import com.kafka.example.producer.AbstractReactiveKafkaProducer
-import org.apache.kafka.clients.producer.ProducerConfig
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import reactor.kafka.sender.SenderResult
-import java.util.UUID
 import java.util.logging.Logger
 
 @Configuration
@@ -16,12 +13,6 @@ class KafkaProducerConfig {
         private val logger = Logger.getLogger(this::class.simpleName)
 
         override val topic = "example-reactive-topic"
-
-        override fun kafkaProducerProperties(): MutableMap<String, Any> =
-            mutableMapOf(
-                Pair(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaServerTestProvider.KAFKA_CONTAINER.bootstrapServers),
-                Pair(ProducerConfig.CLIENT_ID_CONFIG, "client-id-${UUID.randomUUID()}")
-            )
 
         override fun classKey() = "generated-key-${this::class.simpleName}-${this.uuid}"
 
